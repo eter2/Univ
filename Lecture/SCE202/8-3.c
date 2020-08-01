cd77281175314bf33e92fce75d465382e5849afc
eter2 <eter2hy@gmail.com>
2020-08-01 21:08:42 +0900

    Add SCE202

---
+++ Lecture/SCE202/8-3.c
@@ -0,0 +1,152 @@
+﻿// Graph
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+char arr[8] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+int check[7] = { 0 };
+int stack[8];
+int top = -1;
+
+typedef struct Node {
+	char vertex;
+	int num;
+	Node* next;
+} Node;
+
+typedef struct List {
+	Node* head;
+} List;
+
+typedef struct Graph {
+	int v;
+	List* array;
+} Graph;
+
+Node* newNode(int num, char vertex);
+Graph* createGraph(int v);
+void addEdge(Graph* graph, int from, int to);
+void print(Graph* graph);
+
+void push(int v);
+int pop();
+void dfs(Graph* graph, int v);
+
+int main() {
+	int v = 7;
+	Graph* graph = createGraph(v);
+	addEdge(graph, 6, 5);
+	addEdge(graph, 4, 6);
+	addEdge(graph, 3, 6);
+	addEdge(graph, 2, 4);
+	addEdge(graph, 1, 4);
+	addEdge(graph, 1, 3);
+	addEdge(graph, 0, 2);
+	addEdge(graph, 0, 1);
+
+	printf("\n Graph의 adjacency list\n");
+	print(graph);
+	printf("\n");
+
+	printf("\n Depth First Search의 결과: ");
+	dfs(graph, 0);
+	printf("\n");
+
+	return 0;
+}
+
+Node* newNode(int num, char vertex) {
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->num = num;
+	node->vertex = vertex;
+	node->next = NULL;
+	return node;
+}
+
+Graph* createGraph(int v) {
+	Graph* graph = (Graph*)malloc(sizeof(Graph));
+	graph->v = v;
+
+	graph->array = (List*)malloc(v * sizeof(List));
+
+	int i = 0;
+	for (i = 0; i < v; i++)
+		graph->array[i].head = NULL;
+
+	return graph;
+}
+
+void addEdge(Graph* graph, int from, int to) {
+	Node* node = newNode(to, arr[to]);
+	node->next = graph->array[from].head;
+	graph->array[from].head = node;
+
+	node = newNode(from, arr[from]);
+	node->next = graph->array[to].head;
+	graph->array[to].head = node;
+}
+
+void print(Graph* graph) {
+	int v;
+	for (v = 0; v < graph->v; ++v) {
+		Node* node = graph->array[v].head;
+		printf("\n\t vertex %c의 adjacency list", arr[v]);
+		while (node)
+		{
+			printf(" -> %c", node->vertex);
+			node = node->next;
+		}
+	}
+}
+
+void push(int v) {
+	stack[++top] = v;
+}
+
+int pop() {
+	return stack[top--];
+}
+
+void dfs(Graph* graph, int v) {
+	check[v] = 1;
+	printf("%c ", arr[v]);
+	push(v);
+
+	int numTemp = v;
+
+	List temp = graph->array[v];
+	Node* vtxTemp = temp.head;
+
+	while (top != -1) {
+		int idx = -1;
+
+		for (int i = 0; i < 7; i++) {
+			if (top == -1) {
+				return;
+			}
+			if (vtxTemp == NULL)
+			{
+				i = 0;
+				temp = graph->array[pop()];
+				vtxTemp = temp.head;
+			}
+			if (arr[i] == vtxTemp->vertex) {
+				if (check[i] == 1) {
+					i = 0;
+					vtxTemp = vtxTemp->next;
+				}
+				else idx = i;
+			}
+		}
+
+		if (idx == -1) continue;
+		else {
+			check[idx] = 1;
+			printf("%c ", arr[idx]);
+			push(idx);
+			temp = graph->array[idx];
+			vtxTemp = temp.head;
+		}
+	}
+}
---
+++ Lecture/SCE202/9-3.c
@@ -0,0 +1,79 @@
+﻿// Graph
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Edge {
+	int v1;
+	int v2;
+	int weight;
+} Edge;
+
+Edge* edge[12] = { 0 };
+int parent[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+Edge* newEdge(int v1, int v2, int weight);
+void SimpleUnion(int i, int j);
+int SimpleFind(int i);
+void Kruskal();
+
+int main() {
+	edge[0] = newEdge(0, 7, 1);
+	edge[1] = newEdge(1, 5, 2);
+	edge[2] = newEdge(3, 7, 3);
+	edge[3] = newEdge(1, 2, 4);
+	edge[4] = newEdge(4, 5, 5);
+	edge[5] = newEdge(0, 3, 6);
+	edge[6] = newEdge(2, 5, 7);
+	edge[7] = newEdge(4, 7, 8);
+	edge[8] = newEdge(5, 6, 9);
+	edge[9] = newEdge(0, 1, 10);
+	edge[10] = newEdge(2, 3, 11);
+	edge[11] = newEdge(6, 7, 12);
+
+	printf("Kruskal's algorithm을 이용한 minimum cost spanning tree 생성: T={");
+	Kruskal();
+	printf(" }\n");
+
+	return 0;
+}
+
+Edge* newEdge(int v1, int v2, int weight) {
+	Edge* edge = (Edge*)malloc(sizeof(Edge));
+	edge->v1 = v1;
+	edge->v2 = v2;
+	edge->weight = weight;
+
+	return edge;
+}
+
+void SimpleUnion(int i, int j) {
+	while (1) {
+		if (parent[i] == i) break;
+		i = parent[i];
+	}
+
+	parent[i] = j;
+}
+
+int SimpleFind(int i) {
+	while (1) {
+		if (parent[i] == i) break;
+		i = parent[i];
+	}
+
+	return parent[i];
+}
+
+void Kruskal() {
+	for (int i = 0; i < 12; i++) {
+		int root1 = SimpleFind(edge[i]->v1);
+		int root2 = SimpleFind(edge[i]->v2);
+
+		if (root1 != root2) {
+			printf(" (%d,%d)", edge[i]->v1, edge[i]->v2);
+			SimpleUnion(edge[i]->v1, edge[i]->v2);
+		}
+	}
+}
